b011e31d903ba32aff0f212730e2d4b2e6b153a9
Tyler Garlick <tjgarlick@gmail.com>
2018-01-17 20:00:07 -0700

    Added monk and the pets collection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log(`hello world 123`)
+import Monk from 'monk'
+
+const uri = process.env.MONGO_URI
+if(!uri)
+  throw new Error('URI not defined')
+
+const db = Monk(uri)
+const pets = db.get('pets')
+
+const run = async () => {
+  const beavis = await pets.findOne({ name: 'Beavis'})
+
+  if(!beavis)
+    await pets.insert({ name: 'Beavis', type: 'Dog'})
+
+  const fizzgig = await pets.findOne({ name: 'Fizzgig'})
+  if(!fizzgig)
+    await pets.insert({ name: 'Fizzgig', type: 'Dog'})
+
+  const results = await pets.find()
+
+  console.log(results)
+}
+
+
+run()
+  .catch(console.error)
